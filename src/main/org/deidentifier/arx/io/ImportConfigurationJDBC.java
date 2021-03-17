da4f8f0304c89e5c52b6b7dd2eafafc57c952ec1
Mathieu Mailhos <mathieumailhos@gmail.com>
2021-03-17 18:06:00 +1100

    debug: print silent exception and add debug logs

--- src/main/org/deidentifier/arx/io/ImportConfigurationJDBC.java
+++ src/main/org/deidentifier/arx/io/ImportConfigurationJDBC.java
@@ -1,13 +1,13 @@
 /*
  * ARX: Powerful Data Anonymization
  * Copyright 2014 - 2015 Karol Babioch, Fabian Prasser, Florian Kohlmayer
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,176 +30,179 @@ import java.util.NoSuchElementException;
  * @author Fabian Prasser
  */
 public class ImportConfigurationJDBC extends ImportConfiguration {
-    
-    /**
-     * Connection to be used.
-     *
-     * @see {@link #setConnection(Connection)}
-     * @see {@link #getConnection()}
-     */
-    private Connection connection;
-    
-    /**
-     * Name of table to be used.
-     *
-     * @see {@link #setTable(String)}
-     * @see {@link #getTable()}
-     */
-    private String     table;
-    
-    /**
-     * Determines whether we need to manage the JDBC connection.
-     */
-    private final boolean manageConnection;
-    
-    /**
-     * Creates a new instance of this object.
-     *
-     * @param connection {@link #setConnection(Connection)}
-     * @param table {@link #setTable(String)}
-     */
-    public ImportConfigurationJDBC(Connection connection, String table) {
-        this.connection = connection;
-        this.table = table;
-        this.manageConnection = false;
+
+  /**
+   * Connection to be used.
+   *
+   * @see {@link #setConnection(Connection)}
+   * @see {@link #getConnection()}
+   */
+  private Connection connection;
+
+  /**
+   * Name of table to be used.
+   *
+   * @see {@link #setTable(String)}
+   * @see {@link #getTable()}
+   */
+  private String table;
+
+  /**
+   * Determines whether we need to manage the JDBC connection.
+   */
+  private final boolean manageConnection;
+
+  /**
+   * Creates a new instance of this object.
+   *
+   * @param connection {@link #setConnection(Connection)}
+   * @param table      {@link #setTable(String)}
+   */
+  public ImportConfigurationJDBC(Connection connection, String table) {
+    this.connection = connection;
+    this.table = table;
+    this.manageConnection = false;
+  }
+
+  /**
+   * Creates a new instance of this object.
+   *
+   * @param url
+   * @param table {@link #setTable(String)}
+   * @throws SQLException
+   */
+  public ImportConfigurationJDBC(String url, String table) throws SQLException {
+    this.connection = DriverManager.getConnection(url);
+    this.table = table;
+    this.manageConnection = true;
+  }
+
+  /**
+   * Creates a new instance of this object.
+   *
+   * @param url
+   * @param user
+   * @param password
+   * @param table    {@link #setTable(String)}
+   * @throws SQLException
+   */
+  public ImportConfigurationJDBC(String url, String user, String password, String table) throws SQLException {
+    this.connection = DriverManager.getConnection(url, user, password);
+    this.table = table;
+    this.manageConnection = true;
+  }
+
+  /**
+   * Adds a single column to import from
+   * <p>
+   * This makes sure that only {@link ImportColumnJDBC} can be added,
+   * otherwise an {@link IllegalArgumentException} will be thrown.
+   *
+   * @param column A single column to import from, {@link ImportColumnJDBC}
+   */
+  @Override
+  public void addColumn(ImportColumn column) {
+
+    if (!(column instanceof ImportColumnJDBC)) {
+      throw new IllegalArgumentException("");
     }
-    
-    /**
-     * Creates a new instance of this object.
-     *
-     * @param url
-     * @param table {@link #setTable(String)}
-     * @throws SQLException
-     */
-    public ImportConfigurationJDBC(String url, String table) throws SQLException {
-        this.connection = DriverManager.getConnection(url);
-        this.table = table;
-        this.manageConnection = true;
+
+    if (((ImportColumnJDBC) column).getIndex() == -1) {
+      int index = getIndexForColumn(((ImportColumnJDBC) column).getName());
+      ((ImportColumnJDBC) column).setIndex(index);
     }
-    
-    /**
-     * Creates a new instance of this object.
-     *
-     * @param url
-     * @param user
-     * @param password
-     * @param table {@link #setTable(String)}
-     * @throws SQLException
-     */
-    public ImportConfigurationJDBC(String url, String user, String password, String table) throws SQLException {
-        this.connection = DriverManager.getConnection(url, user, password);
-        this.table = table;
-        this.manageConnection = true;
+
+    for (ImportColumn c : columns) {
+
+      if (((ImportColumnJDBC) column).getIndex() == ((ImportColumnJDBC) c).getIndex()) {
+        throw new IllegalArgumentException("Column for this index already assigned");
+      }
+
+      if (column.getAliasName() != null && c.getAliasName() != null &&
+          c.getAliasName().equals(column.getAliasName())) {
+        throw new IllegalArgumentException("Column names need to be unique");
+      }
     }
-    
-    /**
-     * Adds a single column to import from
-     * 
-     * This makes sure that only {@link ImportColumnJDBC} can be added,
-     * otherwise an {@link IllegalArgumentException} will be thrown.
-     * 
-     * @param column
-     *            A single column to import from, {@link ImportColumnJDBC}
-     */
-    @Override
-    public void addColumn(ImportColumn column) {
-        
-        if (!(column instanceof ImportColumnJDBC)) {
-            throw new IllegalArgumentException("");
+    this.columns.add(column);
+  }
+
+  /**
+   * Closes any underlying JDBC connection that may have either been created by ARX or passed during construction.
+   */
+  public void close() {
+    try {
+      this.connection.close();
+    } catch (Exception e) {
+      // Ignore
+    }
+  }
+
+  /**
+   * @param aliasName
+   * @return
+   * @throws NoSuchElementException
+   */
+  private int getIndexForColumn(String aliasName) throws NoSuchElementException {
+    ResultSet rs = null;
+    int index = -1;
+    try {
+
+      rs = connection.getMetaData().getColumns(null,
+          null,
+          table,
+          null);
+
+      int i = 0;
+      while (rs.next()) {
+        String column = rs.getString("COLUMN_NAME");
+        System.out.println("found column " + column + " at index " + i);
+        if (column.equals(aliasName)) {
+          index = i;
         }
-        
-        if (((ImportColumnJDBC) column).getIndex() == -1) {
-            int index = getIndexForColumn(((ImportColumnJDBC) column).getName());
-            ((ImportColumnJDBC) column).setIndex(index);
+        i++;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      ;
+    } finally {
+      try {
+        if (rs != null) {
+          rs.close();
         }
-        
-        for (ImportColumn c : columns) {
-            
-            if (((ImportColumnJDBC) column).getIndex() == ((ImportColumnJDBC) c).getIndex()) {
-                throw new IllegalArgumentException("Column for this index already assigned");
-            }
-            
-            if (column.getAliasName() != null && c.getAliasName() != null &&
-                c.getAliasName().equals(column.getAliasName())) {
-                throw new IllegalArgumentException("Column names need to be unique");
-            }
-        }
-        this.columns.add(column);
+      } catch (SQLException e) {
+        e.printStackTrace();
+        ;
+      }
     }
-    
-    /**
-     * Closes any underlying JDBC connection that may have either been created by ARX or passed during construction.
-     */
-    public void close() {
-        try {
-            this.connection.close();
-        } catch (Exception e) {
-            // Ignore
-        }
+    if (index != -1) {
+      System.out.println("returning index " + index + " for column " + aliasName);
+      return index;
     }
-    
-    /**
-     * 
-     *
-     * @param aliasName
-     * @return
-     * @throws NoSuchElementException
-     */
-    private int getIndexForColumn(String aliasName) throws NoSuchElementException {
-        ResultSet rs = null;
-        int index = -1;
-        try {
-            
-            rs = connection.getMetaData().getColumns(null,
-                                                     null,
-                                                     table,
-                                                     null);
-            
-            int i = 0;
-            while (rs.next()) {
-                if (rs.getString("COLUMN_NAME").equals(aliasName)) {
-                    index = i;
-                }
-                i++;
-            }
-        } catch (SQLException e) {
-            /* Catch silently */
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                /* Ignore silently */
-            }
-        }
-        if (index != -1) {
-            return index;
-        }
-        throw new NoSuchElementException("Index for column '" + aliasName +
-                                         "' couldn't be found");
-        
-    }
-    
-    /**
-     * @return {@link #connection}
-     */
-    protected Connection getConnection() {
-        return this.connection;
-    }
-    
-    /**
-     * @return {@link #table}
-     */
-    protected String getTable() {
-        return this.table;
-    }
-    
-    /**
-     * Returns whether we need to close the connection
-     * @return
-     */
-    protected boolean isManageConnection() {
-        return this.manageConnection;
-    }
+    throw new NoSuchElementException("Index for column '" + aliasName +
+        "' couldn't be found");
+
+  }
+
+  /**
+   * @return {@link #connection}
+   */
+  protected Connection getConnection() {
+    return this.connection;
+  }
+
+  /**
+   * @return {@link #table}
+   */
+  protected String getTable() {
+    return this.table;
+  }
+
+  /**
+   * Returns whether we need to close the connection
+   *
+   * @return
+   */
+  protected boolean isManageConnection() {
+    return this.manageConnection;
+  }
 }
